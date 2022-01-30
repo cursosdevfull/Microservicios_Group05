c7141c086ebcaced12ac10771c0eb8b884ec9b0b
Sergio Hidalgo <shidalgoc@auna.pe>
2022-01-30 15:31:42 -0500

    orquestacion

--- 05-patron-saga-orquestacion/store/src/server.ts
+++ 05-patron-saga-orquestacion/store/src/server.ts
@@ -1,21 +1,26 @@
 import BrokerBootstrap from "./bootstrap/broker.bootstrap";
 import ServerBootstrap from "./bootstrap/server.bootstrap";
+import DatabaseBootstrap from "./bootstrap/database.bootstrap";
 import TasksBootstrap from "./bootstrap/tasks.bootstrap";
 
 import app from "./app";
 import UseCase from "./modules/application/usecase";
 import OperationQueue from "./modules/infraestructure/operation-queue";
+import Operation from "./modules/infraestructure/operation";
 
 (async () => {
   const serverBootstrap = new ServerBootstrap(app);
   const brokerBootstrap = new BrokerBootstrap();
+  const databaseBootstrap = new DatabaseBootstrap();
 
-  const operationQueue = new OperationQueue();
+  const operation = new Operation();
+  const operationQueue = new OperationQueue(operation);
   const useCase = new UseCase(operationQueue);
   const tasksBootstrap = new TasksBootstrap(useCase);
 
   try {
     await serverBootstrap.initialize();
+    await databaseBootstrap.initialize();
     await brokerBootstrap.initialize();
     await tasksBootstrap.listenMessages();
   } catch (error) {
